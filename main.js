fd9d26bb384dafe256fe7a35a17952d2d2c83565
John Schimenz <john.schimenz@gmail.com>
2023-12-21 07:26:48 -0600

    Set up HTML, CSS, and JS files

---
+++ main.js
@@ -0,0 +1,26 @@
+// Projects array
+const projectsArray = [];
+
+// Current project variable
+let currentProject;
+
+// Project factory
+const ProjectFactory = (name) => {
+    name = name;
+    toDos = [];
+    return {name, toDos};
+}
+
+// ToDo arguments array
+const toDoArgumentsArray = ['title', 'description', 'dueDate', 'priority', 'notes', 'checklist'];
+
+// ToDo factory
+const ToDoFactory = (title, description, dueDate, priority, notes, checklist) => {
+    title = title;
+    description = description;
+    dueDate = dueDate;
+    priority = priority;
+    notes = notes;
+    checklist = checklist;
+    return {title, description, dueDate, priority, notes, checklist};
+}
