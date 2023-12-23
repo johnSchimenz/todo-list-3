14eef00ae5a7d8e3128c98b001e6e68798839251
John Schimenz <john.schimenz@gmail.com>
2023-12-23 08:39:16 -0600

    Submit button for project clickable, but now need it to function the way it needs to

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const projectsArray = [];
 let currentProject = "";
 
 // DOM - list of all document.querySelector's
+const selectBottomLeftContainer = document.querySelector('#bottom-left');
+const selectBottomRightContainer = document.querySelector('#bottom-right');
 const selectProjectsContainer = document.querySelector('#list-of-projects-container');
 const selectListOfToDosContainer = document.querySelector('#list-of-todos-container');
-const selectCreateNewProjectButton = document.querySelector('#create-new-project');
-const selectCreateNewToDoItemButton = document.querySelector('#create-new-todo-item');
+const clickCreateNewProjectButton = document.querySelector('#create-new-project');
+const clickCreateNewToDoItemButton = document.querySelector('#create-new-todo-item');
 
 // Project factory
 const ProjectFactory = (name) => {
@@ -52,9 +54,25 @@ const ToDoFactory = (title, description, dueDate, priority, notes, checklist) =>
     return {title, description, dueDate, priority, notes, checklist};
 }
 
-/*
-for (let i = 0; i < projectsArray.length; i++) {
-    const displayProject = document.createElement('button');
-    displayProject.textContent = 
-}
-*/
+// DOM = make Create New Project Button clickable
+clickCreateNewProjectButton.addEventListener('click', () => {
+
+    // Create input box to type in new project title
+    const inputBox = document.createElement('input');
+    inputBox.setAttribute('type', 'text');
+    inputBox.setAttribute('id', 'newest-project');
+    selectBottomLeftContainer.appendChild(inputBox);
+
+    // Create button to submit the inputted project title
+    const submitProjectButton = document.createElement('button');
+    submitProjectButton.textContent = 'Submit';
+    submitProjectButton.setAttribute('id', 'submit');
+    submitProjectButton.setAttribute('type', 'submit');
+    selectBottomLeftContainer.appendChild(submitProjectButton);
+
+    // DOM - make button to submit project clickable
+    const clickSubmitButton = document.querySelector('#submit');
+    clickSubmitButton.addEventListener('click', () => {
+        console.log("submit button works");
+    })
+})
